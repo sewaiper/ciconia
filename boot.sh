0ad41dbf1a63a56a73f2f1c21d3845236c0c8a36
dmitry.chernov <dmitr.chernov@x5.ru>
2025-03-17 19:40:54 +0300

    Поправил скрипт сборки

--- boot.sh
+++ boot.sh
@@ -1,24 +1,24 @@
 #!/bin/bash
 
 JAVA_HOME=/Library/Java/JavaVirtualMachines/jdk-21.jdk/Contents/Home
-TDLIB=/Users/dmitry.chernov/Documents/Projects/news/td
-TG_NEWS=/Users/dmitry.chernov/Documents/Projects/news/tg-news
+TDLIB=~/td
+PROJECT_ROOT=~/ciconia
 
-#cd $TDLIB
-#
-#rm -rf build
-#mkdir build
-#cd build
-#
-#cmake -DCMAKE_BUILD_TYPE=Release \
-#      -DJAVA_HOME=$JAVA_HOME \
-#      -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ \
-#      -DCMAKE_INSTALL_PREFIX:PATH=../example/java/td \
-#      -DTD_ENABLE_JNI=ON ..
-#
-#cmake --build . --target install
+cd $TDLIB
 
-cd $TG_NEWS
+rm -rf build
+mkdir build
+cd build
+
+cmake -DCMAKE_BUILD_TYPE=Release \
+      -DJAVA_HOME=$JAVA_HOME \
+      -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ \
+      -DCMAKE_INSTALL_PREFIX:PATH=../example/java/td \
+      -DTD_ENABLE_JNI=ON ..
+
+cmake --build . --target install
+
+cd $PROJECT_ROOT
 rm -rf tdlib
 mkdir tdlib
 
@@ -30,12 +30,12 @@ cd build
 cmake -DCMAKE_BUILD_TYPE=Release \
       -DJAVA_HOME=$JAVA_HOME \
       -DOPENSSL_ROOT_DIR=/opt/homebrew/opt/openssl/ \
-      -DCMAKE_INSTALL_PREFIX:PATH="${TG_NEWS}/tdlib" \
+      -DCMAKE_INSTALL_PREFIX:PATH="${PROJECT_ROOT}/tdlib" \
       -DTd_DIR:PATH=$(greadlink -e ../td/lib/cmake/Td) ..
 
 cmake --build . --target install
 
-cd "${TG_NEWS}/tdlib"
+cd "${PROJECT_ROOT}/tdlib"
 
 rm -rf jarRoot
 mkdir jarRoot
@@ -44,7 +44,7 @@ cp -r ./bin/org ./jarRoot
 cd jarRoot
 jar cvf ../tdlib.jar *
 
-cd $TG_NEWS
+cd $PROJECT_ROOT
 
 mvn install:install-file -DlocalRepositoryPath=repo \
                          -DcreateChecksum=true \
@@ -54,10 +54,10 @@ mvn install:install-file -DlocalRepositoryPath=repo \
                          -DartifactId=tdlib \
                          -Dversion=1.0
 
-rm -rf "${TG_NEWS}/tdlib/jarRoot"
-rm -f "${TG_NEWS}/tdlib/tdlib.jar"
+rm -rf "${PROJECT_ROOT}/tdlib/jarRoot"
+rm -f "${PROJECT_ROOT}/tdlib/tdlib.jar"
 
-cd "${TG_NEWS}/tdlib"
+cd "${PROJECT_ROOT}/tdlib"
 
 rm -rf docsRoot
 mkdir docsRoot
@@ -66,7 +66,7 @@ cp -r ./docs/org ./docsRoot
 cd docsRoot
 jar cvf ../docs.jar *
 
-cd $TG_NEWS
+cd $PROJECT_ROOT
 
 mvn install:install-file -DlocalRepositoryPath=repo \
                          -DcreateChecksum=true \
@@ -77,7 +77,7 @@ mvn install:install-file -DlocalRepositoryPath=repo \
                          -Dversion=1.0 \
                          -Dclassifier=javadoc
 
-rm -rf "${TG_NEWS}/tdlib/docsRoot"
-rm -f "${TG_NEWS}/tdlib/docs.jar"
+rm -rf "${PROJECT_ROOT}/tdlib/docsRoot"
+rm -f "${PROJECT_ROOT}/tdlib/docs.jar"
 
 
